a2f096bc133a1a4c3e2e0f78f926d296539ad6ce
kaCVanime <kacvanime@gmail.com>
2024-05-20 16:03:42 +0800

    v1

--- __init__.py
+++ __init__.py
@@ -1,41 +1,46 @@
 from aqt import mw
-from aqt.utils import showInfo, qconnect
+from aqt.utils import showInfo
 from aqt.qt import *
+from .progress_gui import ProgressWindow
 
 
 class TransferConfigWindow(QWidget):
     selected_src_deck = ""
     selected_target_deck = ""
-    selected_src_field = ""
-    selected_target_field = ""
     deck_list = []
     model_list = []
-    src_field_list = []
-    target_field_list = []
     rows = []
     transfer_rows = []
+    canceled = False
+    use_transfer_rev_log = False
 
     def __init__(self, deck_name_list, model_list):
         super(TransferConfigWindow, self).__init__()
+
+        self.progress = ProgressWindow(mw)
+
         self.deck_list = deck_name_list
         self.model_list = model_list
 
         self.v_layout = QVBoxLayout(self)
 
-        self.groupbox_deck = QGroupBox('1. Choose Deck')
+        self.groupbox_deck = QGroupBox("1. Choose Deck")
         self.form_layout_deck = QFormLayout(self)
         self.combobox_src_deck = QComboBox(self)
         self.combobox_target_deck = QComboBox(self)
 
         self.scroll_area_rows = QScrollArea(self)
-        self.groupbox_rows = QGroupBox('2. Add group')
+        self.groupbox_rows = QGroupBox("2. Add Group")
         self.v_layout_rows = QVBoxLayout(self)
 
-        self.h_layout_row_btn = QHBoxLayout()
+        self.form_layout_rev_log = QFormLayout(self)
+        self.checkbox_rev_log = QCheckBox(self)
+
+        self.h_layout_row_btn = QHBoxLayout(self)
         self.add_row_btn = QPushButton(self)
-        self.add_row_btn.setText("Add")
+        self.add_row_btn.setText("Add Group")
         self.remove_row_btn = QPushButton(self)
-        self.remove_row_btn.setText("Remove")
+        self.remove_row_btn.setText("Remove Group")
 
         self.start_btn = QPushButton(self)
         self.start_btn.setText("Start")
@@ -51,6 +56,7 @@ class TransferConfigWindow(QWidget):
     def layout_init(self):
         self.v_layout.addWidget(self.groupbox_deck)
         self.v_layout.addWidget(self.scroll_area_rows)
+        self.v_layout.addLayout(self.form_layout_rev_log)
         self.v_layout.addLayout(self.h_layout_row_btn)
         self.v_layout.addWidget(self.start_btn)
         self.v_layout.addWidget(self.console)
@@ -65,6 +71,10 @@ class TransferConfigWindow(QWidget):
         self.scroll_area_rows.setFixedHeight(300)
         self.scroll_area_rows.setMinimumWidth(700)
 
+        self.form_layout_rev_log.addRow(
+            "Transfer Revision history", self.checkbox_rev_log
+        )
+
         self.h_layout_row_btn.addWidget(self.remove_row_btn)
         self.h_layout_row_btn.addWidget(self.add_row_btn)
 
@@ -73,38 +83,42 @@ class TransferConfigWindow(QWidget):
         self.start_btn.clicked.connect(lambda: self.start())
         self.add_row_btn.clicked.connect(lambda: self.add_row())
         self.remove_row_btn.clicked.connect(lambda: self.remove_row())
+        self.checkbox_rev_log.stateChanged.connect(lambda: self.on_rev_log_checked())
 
     def combobox_init(self):
         self.combobox_src_deck.addItems(self.deck_list)
         self.combobox_src_deck.currentIndexChanged.connect(
-            lambda: self.on_deck_selected(self.combobox_src_deck, 'src'))
-        self.on_deck_selected(self.combobox_src_deck, 'src')
+            lambda: self.on_deck_selected(self.combobox_src_deck, "src")
+        )
+        self.on_deck_selected(self.combobox_src_deck, "src")
         self.combobox_target_deck.addItems(self.deck_list)
         self.combobox_target_deck.currentIndexChanged.connect(
-            lambda: self.on_deck_selected(self.combobox_target_deck, 'target'))
-        self.on_deck_selected(self.combobox_target_deck, 'target')
+            lambda: self.on_deck_selected(self.combobox_target_deck, "target")
+        )
+        self.on_deck_selected(self.combobox_target_deck, "target")
 
     def remove_row(self):
         if len(self.rows) > 1:
             row = self.rows.pop()
             self.transfer_rows.pop()
             self.v_layout_rows.removeWidget(row)
+            row.deleteLater()
 
     def add_row(self):
-        groupbox_row = QGroupBox('transfer group')
+        groupbox_row = QGroupBox("transfer group")
         h_layout_row = QHBoxLayout()
 
-        groupbox_row_model = QGroupBox('Choose note type')
+        groupbox_row_model = QGroupBox("1) Choose note type")
         form_layout_row_model = QFormLayout(self)
         combobox_src_model = QComboBox(self)
         combobox_target_model = QComboBox(self)
 
-        groupbox_row_compare = QGroupBox('Choose comparing field')
+        groupbox_row_compare = QGroupBox("2) Choose comparing field")
         form_layout_row_compare = QFormLayout(self)
         combobox_src_compare_field = QComboBox(self)
         combobox_target_compare_field = QComboBox(self)
 
-        groupbox_row_card = QGroupBox('Choose card template')
+        groupbox_row_card = QGroupBox("3) Choose card template")
         form_layout_row_card = QFormLayout(self)
         combobox_src_card = QComboBox(self)
         combobox_target_card = QComboBox(self)
@@ -115,55 +129,90 @@ class TransferConfigWindow(QWidget):
         h_layout_row.addWidget(groupbox_row_card)
 
         groupbox_row_model.setLayout(form_layout_row_model)
-        form_layout_row_model.addRow("src", combobox_src_model)
+        form_layout_row_model.addRow("source", combobox_src_model)
         form_layout_row_model.addRow("target", combobox_target_model)
 
         groupbox_row_compare.setLayout(form_layout_row_compare)
-        form_layout_row_compare.addRow("src", combobox_src_compare_field)
+        form_layout_row_compare.addRow("source", combobox_src_compare_field)
         form_layout_row_compare.addRow("target", combobox_target_compare_field)
 
         groupbox_row_card.setLayout(form_layout_row_card)
-        form_layout_row_card.addRow("src", combobox_src_card)
+        form_layout_row_card.addRow("source", combobox_src_card)
         form_layout_row_card.addRow("target", combobox_target_card)
 
         combobox_src_model.addItems(self.model_list)
         combobox_src_model.currentIndexChanged.connect(
-            lambda: self.on_model_selected(combobox_src_model, combobox_src_compare_field, combobox_src_card))
+            lambda: self.on_model_selected(
+                combobox_src_model, combobox_src_compare_field, combobox_src_card
+            )
+        )
         combobox_target_model.addItems(self.model_list)
         combobox_target_model.currentIndexChanged.connect(
-            lambda: self.on_model_selected(combobox_target_model, combobox_target_compare_field, combobox_target_card))
+            lambda: self.on_model_selected(
+                combobox_target_model,
+                combobox_target_compare_field,
+                combobox_target_card,
+            )
+        )
 
         combobox_src_compare_field.currentIndexChanged.connect(
-            lambda: self.on_transfer_field_selected(groupbox_row, combobox_src_compare_field, "src_compare_field"))
+            lambda: self.on_transfer_field_selected(
+                groupbox_row, combobox_src_compare_field, "src_compare_field"
+            )
+        )
         combobox_target_compare_field.currentIndexChanged.connect(
-            lambda: self.on_transfer_field_selected(groupbox_row, combobox_target_compare_field,
-                                                    "target_compare_field"))
+            lambda: self.on_transfer_field_selected(
+                groupbox_row, combobox_target_compare_field, "target_compare_field"
+            )
+        )
 
         combobox_src_card.currentIndexChanged.connect(
-            lambda: self.on_transfer_field_selected(groupbox_row, combobox_src_card, "src_card"))
+            lambda: self.on_transfer_field_selected(
+                groupbox_row, combobox_src_card, "src_card"
+            )
+        )
         combobox_target_card.currentIndexChanged.connect(
-            lambda: self.on_transfer_field_selected(groupbox_row, combobox_target_card, "target_card"))
+            lambda: self.on_transfer_field_selected(
+                groupbox_row, combobox_target_card, "target_card"
+            )
+        )
 
         self.v_layout_rows.addWidget(groupbox_row)
         self.rows.append(groupbox_row)
         self.transfer_rows.append(
-            {"src_compare_field": "", "target_compare_field": "", "src_card": "", "target_card": ""})
-        self.on_model_selected(combobox_src_model, combobox_src_compare_field, combobox_src_card)
-        self.on_model_selected(combobox_target_model, combobox_target_compare_field, combobox_target_card)
+            {
+                "src_compare_field": "",
+                "target_compare_field": "",
+                "src_card": "",
+                "target_card": "",
+            }
+        )
+        self.on_model_selected(
+            combobox_src_model, combobox_src_compare_field, combobox_src_card
+        )
+        self.on_model_selected(
+            combobox_target_model, combobox_target_compare_field, combobox_target_card
+        )
 
-    def on_deck_selected(self, combobox, type):
+    def on_deck_selected(self, combobox, t):
         label = combobox.currentText()
-        if type == 'src':
+        if t == "src":
             self.selected_src_deck = label
         else:
             self.selected_target_deck = label
+
+    def on_rev_log_checked(self):
+        if self.checkbox_rev_log.isChecked():
+            self.use_transfer_rev_log = True
+        else:
+            self.use_transfer_rev_log = False
 
     def on_model_selected(self, model_box, compare_box, card_box):
         model_name = model_box.currentText()
         if model_name:
             model = mw.col.models.by_name(model_name)
             fields = mw.col.models.field_names(model)
-            templates = [m["name"] for m in model['tmpls']]
+            templates = [m["name"] for m in model["tmpls"]]
 
             compare_box.clear()
             compare_box.addItems(fields)
@@ -177,37 +226,142 @@ class TransferConfigWindow(QWidget):
             index = self.rows.index(row)
             self.transfer_rows[index][name] = field
 
-    def transfer(self, **kwargs):
-        self.console.append(f'query: deck:{self.selected_src_deck} card:{kwargs["src_card"]}')
-        src_card_ids = mw.col.find_cards(f'deck:{self.selected_src_deck} card:{kwargs["src_card"]}')
-        self.console.append(f'{len(src_card_ids)}')
+    def transfer_card_info(self, src_card, target_cards):
+        if len(target_cards):
+            card_fields = [
+                "type",
+                "queue",
+                "due",
+                "ivl",
+                "factor",
+                "reps",
+                "lapses",
+                "left",
+                "odue",
+                "flags",
+                "data",
+            ]
+            target_cids = [getattr(card, "id") for card in target_cards]
+            sql = """
+                UPDATE cards
+                SET {f1}
+                FROM (
+                    SELECT {fields} FROM cards
+                    WHERE id=? 
+                ) as c1
+                WHERE id IN ({tcids})
+            """.format(
+                f1=", ".join([f"{key}=c1.{key}" for key in card_fields]),
+                fields=", ".join(card_fields),
+                tcids=", ".join(map(str, target_cids)),
+            )
+
+            mw.col.db.execute(sql, getattr(src_card, "id"))
+
+    def try_sql(self, sql):
+        try:
+            mw.col.db.execute(sql)
+            return True
+        except Exception:
+            return False
+
+    def transfer_revlog(self, src_card, target_cards):
+        if len(target_cards):
+            columns = ["id", "ease", "ivl", "lastIvl", "factor", "time", "type"]
+            sql1 = "select {} from revlog where cid = ?".format(", ".join(columns))
+            rev_result = mw.col.db.all(sql1, getattr(src_card, "id"))
+
+            rows_delta = [0] * len(rev_result)
+            for offset, card in enumerate(target_cards):
+                cid = getattr(card, "id")
+
+                delete_sql = "DELETE FROM revlog WHERE cid=?"
+                mw.col.db.execute(delete_sql, cid)
+
+                for idx, row in enumerate(map(list, rev_result)):
+                    if rows_delta[idx] > 10:
+                        self.console.append(
+                            f'Transfer revision history failed. src cid: {getattr(src_card, "id")}. target cid: {cid}. {idx+1}th rev log'
+                        )
+                        continue
+
+                    success = False
+                    while not success and rows_delta[idx] <= 10:
+                        sql = "INSERT INTO revlog(id,cid,usn,ease,ivl,lastIvl,factor,time,type) VALUES "
+                        row[1:1] = [cid, -1]
+                        row[0] = int(row[0]) + 1 + offset + rows_delta[idx]
+                        sql += "(%s)" % ",".join(map(str, row))
+                        success = self.try_sql(sql)
+                        if not success:
+                            rows_delta[idx] = rows_delta[idx] + 1
+                    if not success:
+                        self.console.append(
+                            f'Transfer revision history failed. src cid: {getattr(src_card, "id")}. target cid: {cid}. {idx+1}th rev log'
+                        )
+
+    def update_progress(self, count):
+        self.progress.update_labels(count)
+        mw.app.processEvents()
+
+    def transfer(
+        self,
+        title,
+        src_card_tmpl,
+        target_card_tmpl,
+        src_compare_field,
+        target_compare_field,
+    ):
+        src_card_ids = mw.col.find_cards(
+            f"deck:{self.selected_src_deck} card:{src_card_tmpl} -is:new"
+        )
+        self.console.append(f"found {len(src_card_ids)} studied cards")
 
         if len(src_card_ids):
-            card = mw.col.get_card(src_card_ids[0])
-            # result = mw.col.db.first('select * from revlog where cid= ?', src_card_ids[0])
-            # result2 = mw.col.db.first('select * from cards where id= ?', src_card_ids[0])
-            card_fields = ["type", "queue", "due", "ivl", "factor", "reps", "lapses", "left", "odue", "flags", "custom_data"]
-            revlog_fields = ["ease", "ivl", "lastIvl", "factor", "time", "type"]
-            revlog_fields_need_modified = ['id', 'cid', 'usn']
-            result = mw.col.db.first('select {} from cards where id= ?'.format(', '.join(card_fields)), src_card_ids[0])
-            card_fields = [getattr(card, f, None) for f in card_fields]
-            breakpoint()
+            self.progress.start(len(src_card_ids), 0)
+            self.progress.update_title(title)
+            for idx, cid in enumerate(src_card_ids):
+                if self.progress.abort():
+                    self.canceled = True
+                    self.console.append("Canceld")
+                    break
 
+                self.update_progress(idx + 1)
+
+                card = mw.col.get_card(cid)
+                query = f"deck:{self.selected_target_deck} card:{target_card_tmpl} {target_compare_field}:{card.note()[src_compare_field]}"
+                target_card_ids = mw.col.find_cards(query)
+                target_cards = [mw.col.get_card(d) for d in target_card_ids]
+
+                self.transfer_card_info(card, target_cards)
+                if self.use_transfer_rev_log:
+                    self.transfer_revlog(card, target_cards)
 
     def start(self):
         self.console.clear()
+        self.canceled = False
+        self.console.append("Start")
         for idx, row in enumerate(self.transfer_rows):
-            if all(value != '' for value in row.values()):
-                self.console.append(
-                    f'Working on group {idx+1}')
-                self.transfer(src_card=row["src_card"])
+            if self.canceled:
+                break
+            if all(value != "" for value in row.values()):
+                self.console.append(f"Processing Group {idx+1}")
+                self.transfer(
+                    f"Processing Group {idx+1}",
+                    row["src_card"],
+                    row["target_card"],
+                    row["src_compare_field"],
+                    row["target_compare_field"],
+                )
+                self.progress.finish()
             else:
                 self.console.append(
-                    f'Skipping group {idx+1} because it has empty field'
+                    f"Skipping group {idx+1} because it has empty field"
                 )
+        if not self.canceled:
+            self.console.append("Done")
 
 
-def showWindow():
+def show_window():
     decks = mw.col.decks.allNames()
     models = [n.name for n in mw.col.models.all_names_and_ids()]
     mw.transferConfigWindow = TransferConfigWindow(decks, models)
@@ -217,6 +371,6 @@ def showWindow():
 # create a new menu item, "test"
 action = QAction("Transfer Scheduling Data", mw)
 # set it to call testFunction when it's clicked
-action.triggered.connect(showWindow)
+action.triggered.connect(show_window)
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
---
+++ progress_gui.py
@@ -0,0 +1,118 @@
+# -*- coding:utf-8 -*-
+#
+# https://github.com/sirius-fan/FastWordQuery/blob/master/src/gui/progress.py
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version; http://www.gnu.org/copyleft/gpl.html.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import time
+
+from aqt.qt import *
+
+
+__all__ = ["ProgressWindow"]
+
+_INFO_TEMPLATE = "".join(
+    [
+        "<strong>" + "transferring" + "</strong>",
+        "<p>" + 45 * "-" + "</p>",
+        "<p>" + "{}" + " processed" + "</p>",
+    ]
+)
+
+
+class ProgressWindow(object):
+    """
+    Query progress window
+    """
+
+    def __init__(self, mw):
+        self.mw = mw
+        self.app = QApplication.instance()
+        self._win = None
+        self._count = 0
+        self._last_update = 0
+        self._first_time = 0
+        self._disabled = False
+
+    def update_labels(self, count):
+        if self.abort():
+            return
+
+        self._count = count
+
+        number_info = _INFO_TEMPLATE.format(self._count)
+        self._update(number_info, self._count)
+        self._win.adjustSize()
+        self.app.processEvents()
+
+    def update_title(self, title):
+        if self.abort():
+            return
+        self._win.setWindowTitle(title)
+
+    def start(self, max=0, min=0, label=None, parent=None):
+        self._count = 0
+        # setup window
+        label = label or "Processing..."
+        parent = parent or self.app.activeWindow() or self.mw
+        self._win = QProgressDialog(label, "", min, max, parent)
+        self._win.setWindowModality(Qt.WindowModality.ApplicationModal)
+        self._win.setCancelButton(None)
+        self._win.canceled.connect(self.finish)
+        self._win.setWindowTitle("Processing")
+        # TODO
+        self._win.setModal(True)
+        self._win.setWindowFlags(
+            self._win.windowFlags() & ~Qt.WindowType.WindowContextHelpButtonHint
+        )
+        self._win.setAutoReset(True)
+        self._win.setAutoClose(True)
+        self._win.setMinimum(0)
+        self._win.setMaximum(max)
+        # we need to manually manage minimum time to show, as qt gets confused
+        # by the db handler
+        # self._win.setMinimumDuration(100000)
+        self._first_time = time.time()
+        self._last_update = time.time()
+        self._disabled = False
+        self._win.show()
+        self._win.setValue(0)
+        self.app.processEvents()
+
+    def abort(self):
+        # self.aborted = True
+        return self._win.wasCanceled()
+
+    def finish(self):
+        self._win.hide()
+        self._unset_busy()
+        self._win.destroy()
+
+    def _update(self, label, value, process=True):
+        elapsed = time.time() - self._last_update
+        if label:
+            self._win.setLabelText(label)
+        if value:
+            self._win.setValue(value)
+        if process and elapsed >= 0.2:
+            self.app.processEvents(QEventLoop.ProcessEventsFlag.ExcludeUserInputEvents)
+            self._last_update = time.time()
+
+    def _set_busy(self):
+        self._disabled = True
+        self.mw.app.setOverrideCursor(QCursor(Qt.WaitCursor))
+
+    def _unset_busy(self):
+        self._disabled = False
+        self.app.restoreOverrideCursor()
